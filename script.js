d5d6252849a8098c42797517e8fc1160da847054
Sajeel56 <sajeeltasawar69@gmail.com>
2026-02-12 19:01:24 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function checkAnswer(answer) {
-    const result = document.getElementById('result');
-    if(answer === 'correct') {
-        result.textContent = 'Correct! Biceps Brachii flexes the elbow.';
